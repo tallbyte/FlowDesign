5cf6626171e8a2477ec883c7ee7e9cf19141adba
Julian Klissenbauer-Mathae <julian@klissenbauer.net>
2016-10-26 22:36:57 +0200

    Added simple dragable test-elements

--- javafx/src/main/java/com/tallbyte/flowdesign/javafx/pane/ApplicationPane.java
+++ javafx/src/main/java/com/tallbyte/flowdesign/javafx/pane/ApplicationPane.java
@@ -1,8 +1,16 @@
 package com.tallbyte.flowdesign.javafx.pane;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.LoadException;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
@@ -14,6 +22,9 @@ import java.io.IOException;
  */
 public class ApplicationPane extends BorderPane {
 
+    @FXML private Button buttonTest;
+    @FXML private ImageView imageView;
+
     public ApplicationPane() throws LoadException {
         FXMLLoader loader = new FXMLLoader( getClass().getResource("/fxml/applicationPane.fxml") );
         loader.setController(this);
@@ -24,6 +35,20 @@ public class ApplicationPane extends BorderPane {
         } catch (IOException e) {
             throw new LoadException("Could not load "+getClass().getSimpleName());
         }
+
+        buttonTest.setOnDragDetected(event -> {
+            buttonTest.startFullDrag();
+            Dragboard db = buttonTest.startDragAndDrop(TransferMode.COPY);
+            SnapshotParameters params = new SnapshotParameters();
+            params.setFill(Color.TRANSPARENT);
+            db.setDragView(buttonTest.snapshot(params, null));
+            db.setDragViewOffsetX(event.getX());
+            db.setDragViewOffsetY(event.getY());
+            ClipboardContent content = new ClipboardContent();
+            content.putString(buttonTest.getId());
+            db.setContent(content);
+            event.consume();
+        });
     }
 
 }
