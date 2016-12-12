86db8bfb60ae04e581384f0989c5886fccb6ac77
Julian Klissenbauer-Mathae <julian@klissenbauer.net>
2016-12-12 16:39:35 +0100

    More visuals

--- javafx/src/main/java/com/tallbyte/flowdesign/javafx/diagram/image/VerticalStickDiagramImage.java
+++ javafx/src/main/java/com/tallbyte/flowdesign/javafx/diagram/image/VerticalStickDiagramImage.java
@@ -33,7 +33,7 @@ public class VerticalStickDiagramImage extends DiagramImage {
      * Creates a new {@link VerticalStickDiagramImage} with default dimension.
      */
     public VerticalStickDiagramImage() {
-
+        setWidth(10);
     }
 
     @Override
