e8c5ce7401449f458959546661c902c9edcd8c57
Julian Klissenbauer-Mathae <julian@klissenbauer.net>
2017-01-05 20:38:54 +0100

    Fixed join navigation

--- javafx/src/main/java/com/tallbyte/flowdesign/javafx/diagram/element/JoinElementNode.java
+++ javafx/src/main/java/com/tallbyte/flowdesign/javafx/diagram/element/JoinElementNode.java
@@ -20,6 +20,7 @@ package com.tallbyte.flowdesign.javafx.diagram.element;
 
 import com.tallbyte.flowdesign.data.Connection;
 import com.tallbyte.flowdesign.data.FlowJoint;
+import com.tallbyte.flowdesign.data.Joint;
 import com.tallbyte.flowdesign.data.flow.Join;
 import com.tallbyte.flowdesign.javafx.ShortcutGroup;
 import com.tallbyte.flowdesign.javafx.Shortcuts;
@@ -27,6 +28,7 @@ import com.tallbyte.flowdesign.javafx.diagram.ElementNode;
 import com.tallbyte.flowdesign.javafx.diagram.image.DiagramImage;
 import javafx.geometry.Pos;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -92,8 +94,11 @@ public class JoinElementNode extends ElementNode {
         });
 
         group.getShortcut(Shortcuts.SHORTCUT_MOVE_UP).setAction(event -> {
-            List<Connection> list = join.getInputGroup().getJoint(0)
-                    .getIncoming().stream().collect(Collectors.toList());
+            List<Connection> list = new ArrayList<>();
+
+            for (Joint j : join.getInputGroup().getJoints()) {
+                list.addAll(j.getIncoming());
+            }
 
             if (list.size() > 0) {
                 list.sort((o1, o2) -> (int) (o1.getSource().getElement().getY() - o2.getSource().getElement().getY()));
@@ -104,8 +109,11 @@ public class JoinElementNode extends ElementNode {
         });
 
         group.getShortcut(Shortcuts.SHORTCUT_MOVE_DOWN).setAction(event -> {
-            List<Connection> list = join.getInputGroup().getJoint(0)
-                    .getIncoming().stream().collect(Collectors.toList());
+            List<Connection> list = new ArrayList<>();
+
+            for (Joint j : join.getInputGroup().getJoints()) {
+                list.addAll(j.getIncoming());
+            }
 
             if (list.size() > 0) {
                 list.sort((o1, o2) -> (int) (o2.getSource().getElement().getY() - o1.getSource().getElement().getY()));
